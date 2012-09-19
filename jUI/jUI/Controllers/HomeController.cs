f0be508536abb3b9108067fdb2a7ab9cb4bff640
sorex <sorex1984@gmail.com>
2012-09-19 09:55:46 +0800

    添加新的文件
    添加控件对应文件
    button
    checkbox
    radio
    headMenu
    leftMenu
    table

--- jUI/jUI/Controllers/HomeController.cs
+++ jUI/jUI/Controllers/HomeController.cs
@@ -12,5 +12,57 @@ namespace jUI.Controllers
 		{
 			return View();
 		}
+
+		public ActionResult leftMenu()
+		{
+			return View();
+		}
+
+		public ActionResult leftMenuHtml()
+		{
+			return View();
+		}
+
+		public ActionResult button()
+		{
+			return View();
+		}
+		public ActionResult buttonHtml()
+		{
+			return View();
+		}
+		public ActionResult checkbox()
+		{
+			return View();
+		}
+		public ActionResult checkboxHtml()
+		{
+			return View();
+		}
+		public ActionResult radio()
+		{
+			return View();
+		}
+		public ActionResult radioHtml()
+		{
+			return View();
+		}
+		public ActionResult headMenu()
+		{
+			return View();
+		}
+		public ActionResult headMenuHtml()
+		{
+			return View();
+		}
+		public ActionResult table()
+		{
+			return View();
+		}
+		public ActionResult tableHtml()
+		{
+			return View();
+		}
+
 	}
 }
