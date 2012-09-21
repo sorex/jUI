5075c8a65b3a25f3a74e358fb7e4479fbe8991aa
sorex <sorex1984@gmail.com>
2012-09-21 17:42:38 +0800

    调整Standards位置
    将Standards调整到Standards文件夹下，以前的Home/Index页面仅做连接

---
+++ jUI/jUI/Controllers/StandardsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jUI.Controllers
+{
+    public class StandardsController : Controller
+    {
+        //
+        // GET: /Standards/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Basic()
+		{
+			return View();
+		}
+    }
+}
