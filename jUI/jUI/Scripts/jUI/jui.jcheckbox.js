9893f9f757751c8db6f972979be8c9474632f4c5
Leidc <l15871437346@163.com>
2012-10-11 15:14:10 +0800

    checkbox.cshtml页面直接引用css 和js的修改

--- jUI/jUI/Scripts/jUI/jui.jcheckbox.js
+++ jUI/jUI/Scripts/jUI/jui.jcheckbox.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(function () {
+	$(":checkbox").after("<span class='jui-checkbox'></span>").click(function (event) {
+		if ($(this).is(':checked')) {
+			$(this).next().addClass("jui-checkbox-checked");
+			$(this).checked = true;
+		}
+		else {
+			$(this).next().removeClass('jui-checkbox-checked');
+			$(this).checked = false;
+		}
+		event.stopPropagation();
+	}).hide().next().click(function (event) {
+		var myObject = document.getElementById($(this).prev().attr("id"));
+		if (myObject.checked == true) {
+			$(this).removeClass('jui-checkbox-checked')
+			myObject.checked = false;
+		} else {
+
+			myObject.checked = true;
+			$(this).addClass('jui-checkbox-checked')
+		}
+		event.stopPropagation();
+	});
+
+});
