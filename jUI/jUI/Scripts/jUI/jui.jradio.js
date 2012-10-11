22ae9a2f74e6071fbc8f3aed382162b7d15dcb1e
Leidc <l15871437346@163.com>
2012-10-11 15:15:29 +0800

    radio的html页面 和 直接通过引用样式和js达到效果的修改

--- jUI/jUI/Scripts/jUI/jui.jradio.js
+++ jUI/jUI/Scripts/jUI/jui.jradio.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(function () {
+	$(":radio").after("<span class='jui-radio'></span>").click(function () {
+		if ($(this).is(':checked')) {
+			$(this).next().siblings().removeClass("jui-radio-checked");
+			$(this).next().addClass("jui-radio-checked");
+			$(this).checked = true;
+		} else {
+			$(this).next().removeClass("jui-radio-checked");
+			$(this).checked = false;
+
+		}
+	}).hide().next().click(function () {
+		$(this).siblings().removeClass("jui-radio-checked");
+		if (!$(this).prev().is(":checked")) {
+			$(this).prev()[0].checked = true;
+			$(this).addClass("jui-radio-checked");
+		}
+	});
+
+});
