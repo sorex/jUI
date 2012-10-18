8a98087868cdd2d085abe417d9aa3b136b2c7581
sorex <sorex1984@gmail.com>
2012-10-18 17:54:44 +0800

    调整了jUI的image位置以及BundleConfig

--- jUI/jUI/App_Start/BundleConfig.cs
+++ jUI/jUI/App_Start/BundleConfig.cs
@@ -8,35 +8,44 @@ namespace jUI
 		// 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/script/basic").Include(
-						"~/Scripts/jquery-{version}.js",
-						"~/Scripts/jquery-ui-{version}.js",
-						"~/Scripts/jquery.validate*",
-						"~/Scripts/modernizr-*",
-						"~/Scripts/jUI/jui.jlayout.js"
-						));
+			bundles.Add(new ScriptBundle("~/Scripts/basic").Include(
+				"~/Scripts/jquery-{version}.js",
+				"~/Scripts/jquery-ui-{version}.js",
+				"~/Scripts/jquery.validate*",
+				"~/Scripts/modernizr-*"
+				));
 
-			bundles.Add(new StyleBundle("~/css/basic").Include(
-						"~/Content/reset.css",
-						"~/Content/site.css",
-						"~/Content/jquery-ui.css",
-						"~/Content/jUI/jui.jlayout.css"
-						));
+			bundles.Add(new StyleBundle("~/Content/basic").Include(
+				"~/Content/reset.css",
+				"~/Content/site.css",
+				"~/Content/jquery-ui.css"
+				));
 
 			//添加syntaxhighlighter相关的包
-			bundles.Add(new ScriptBundle("~/script/syntaxhighlighter").Include(
-						"~/Scripts/syntaxhighlighter/shCore.js",
-						"~/Scripts/syntaxhighlighter/shAutoloader.js",
-						"~/Scripts/syntaxhighlighter/shBrushXml.js",
-						"~/Scripts/syntaxhighlighter/shBrushJScript.js",
-						"~/Scripts/syntaxhighlighter/shBrushCSharp.js",
-						"~/Scripts/syntaxhighlighter/shBrushCss.js",
-						"~/Scripts/syntaxhighlighter/configSH.js"
-						));
-			bundles.Add(new StyleBundle("~/css/syntaxhighlighter").Include(
+			bundles.Add(new ScriptBundle("~/Scripts/syntaxhighlighter/js").Include(
+				"~/Scripts/syntaxhighlighter/shCore.js",
+				"~/Scripts/syntaxhighlighter/shAutoloader.js",
+				"~/Scripts/syntaxhighlighter/shBrushXml.js",
+				"~/Scripts/syntaxhighlighter/shBrushJScript.js",
+				"~/Scripts/syntaxhighlighter/shBrushCSharp.js",
+				"~/Scripts/syntaxhighlighter/shBrushCss.js",
+				"~/Scripts/syntaxhighlighter/configSH.js"
+				));
+			bundles.Add(new StyleBundle("~/Content/syntaxhighlighter/css").Include(
 				"~/Content/syntaxhighlighter/shCoreDefault.css",
-				"~/Content/syntaxhighlighter/shThemeDefault.css"));
+				"~/Content/syntaxhighlighter/shThemeDefault.css"
+				));
 
+			//添加jUI相关的包
+			bundles.Add(new ScriptBundle("~/Scripts/jUI/files").Include(
+				"~/Scripts/jUI/jui.jlayout.js",
+				"~/Scripts/jUI/jui.jheadMenu.js"
+				));
+
+			bundles.Add(new StyleBundle("~/Content/jUI/files").Include(
+				"~/Content/jUI/jui.jlayout.css",
+				"~/Content/jUI/jui.jheadMenu.css"
+				));
 		}
 	}
 }
