84e8cb6e48af09ae53f946b990fa52bc14e4e88b
zjjs <294358340@qq.com>
2012-10-23 16:23:52 +0800

    table更新

--- jUI/jUI/Scripts/jUI/jui.jtable.js
+++ jUI/jUI/Scripts/jUI/jui.jtable.js
@@ -16,20 +16,61 @@
 * Description: 表格生成插件
 */
 
+/*
+items:
+    [
+        {item,item...},
+        {item,item...},
+        ...
+    ]	//使用item列表为其赋值
+
+    item:
+            head: "名称名称名称名称",				                        //名称
+            context: "#Name(#Name2)(#ID)",			                //#Name(#Name2)(#ID)
+            maxLength: 5,			                                            //文本最大长度，0不限制
+            head_align: "left",                                                 //表头单元格横向分布 [left], center, right
+            v_align: "top",		                                                //表格单元格纵向分布 [top], middle, bottom
+            h_align: "left",		                                                //表格单元格横向分布 [left], center, right
+            height: "auto",			                                            //单元格高度 150px, 10%
+            width: "auto"			                                            //单元格宽度 150px, 10%
+*/
+
 (function ($, undefined) {
     $.widget("j.jtable",
 	{
 	    // default options
 	    options: {
-	        tableHeight: '30%', //表高
-	        tableWidth: '100%', //表宽
-	        data: [], //数据源
-	        columns: {},//列名
-	        overColor: '',//鼠标覆盖后行颜色
-            outColor: '',//鼠标移开后行颜色
-            onClickColor: '',//鼠标点击行后，行颜色
-            onClick: true,//是否开启鼠标点击
-	        noResultsText:''//错误提示
+	        height: '', //表高
+	        width: '', //表宽
+	        data: [
+                        //{ "ID": 1, "Name": "AAAAA", "Name2": "aaaaa", "Name3": "aaaaa3", "Name4": "aaaaaa4", "Name5": "aaaaaaaa5" },
+                        //{ "ID": 2, "Name": "BBBBBBB", "Name2": "bbbbb", "Name3": "bbbbb3", "Name4": "bbbbb4", "Name5": "bbbbb5" },
+                        //{ "ID": 3, "Name": "CCCCCC", "Name2": "cccccc", "Name3": "cccccc3", "Name4": "cccccc4", "Name5": "cccccc5" },
+                        //{ "ID": 4, "Name": "DDDDDD", "Name2": "dddddd", "Name3": "dddddd3", "Name4": "dddddd4", "Name5": "dddddd5" },
+                        //{ "ID": 5, "Name": "EEEEEEE", "Name2": "eeeeeee", "Name3": "eeeeeee3", "Name4": "eeeeeee4", "Name5": "eeeeeee5" },
+                        //{ "ID": 6, "Name": "FFFFFFF", "Name2": "ffffffff", "Name3": "ffffffff3", "Name4": "ffffffff4", "Name5": "ffffffff5" },
+                        //{ "ID": 7, "Name": "GGGGG", "Name2": "gggggg", "Name3": "gggggg3", "Name4": "gggggg4", "Name5": "gggggg5" }
+	        ], //数据源
+	        columns: [
+		                //{
+		                //    head: "名称1",				                                    //名称
+		                //    context: "#Name(#Name2)(#ID)",			            //#Name(#Name2)(#ID)
+		                //    maxLength: 5,			                                        //文本最大长度，0不限制
+		                //    head_align: "left",                                             //[left], center, right
+		                //    v_align: "top",		                                            //[top], middle, bottom
+		                //    h_align: "left",		                                            //[left], center, right
+		                //    height: "auto",			                                        //150px, 10%
+		                //    width: "auto"			                                        //150px, 10%
+		                //},
+		                //{ head: "名称2", context: "#Name2", maxLength: 5, head_align: "left", v_align: "top", h_align: "left", height: "auto", width: "auto" },
+		                //{ head: "名称3", context: "#Name3", maxLength: 5, head_align: "left", v_align: "top", h_align: "left", height: "auto", width: "auto" },
+		                //{ head: "名称4", context: "#Name4", maxLength: 5, head_align: "left", v_align: "top", h_align: "left", height: "auto", width: "auto" },
+		                //{ head: "名称5", context: "#Name5", maxLength: 5, head_align: "left", v_align: "top", h_align: "left", height: "auto", width: "auto" },
+		                //{
+		                //    head: "操作", context: "<a href='#' onclick=ActionEdit(#ID,'#Name','#Name2')>Edit</a>|<a href='#' onclick=ActionDetail(#ID,'#Name','#Name2')>Detail</a>|<a href='#' onclick=ActionDelete(#ID,'#Name','#Name2')>Delete</a>",
+		                //    maxLength: 5, head_align: "left", v_align: "top", h_align: "left", height: "auto", width: "auto"
+		                //}
+	        ]//列
 	    },
 
 	    _create: function () {
@@ -40,93 +81,149 @@
 	        var self = this,
             o = this.options;
 	        $(this.element).empty();
-	        var image = "../../Images/orderedList0.png";
-	        $(document).bind("selectstart", function () { 
-	            var image = "../../Images/headMenuTestImage.png";return false; }); //控制文本不能被选中
+	        $(document).bind("selectstart", function () { return false; }); //控制文本不能被选中
+	        //无数据时，显示错误提示信息
 	        if (o.data.length == 0) {
-	            $(this.element).empty().append('<span class="ui-icon ui-icon-alert" style="float: left; margin: 3px 3px 0px 3px;"></span><p>' + o.noResultsText + '</p>');
+	            $(this.element).empty().append('<span class="ui-icon ui-icon-alert" style="float: left; margin: 3px 3px 0px 3px;"></span><p>' + 无数据 + '</p>');
 	        } else {
 	            $(this.element).empty().addClass("jui-table");
 	            var tableId = $(this.element).attr("id");
-	            $(this.element).append("<table id='" + tableId + "_tableSorter' width='" + o.tableWidth + "' height='" + o.tableHeight + "'><thead><tr></tr></thead><tbody></tbody></table><span id='dis_checkedItems' style='display:none;'></span>");
+
+                //生成表框架
+	            $(this.element).append(
+                    "<table id='" + tableId + "_tableSorter' style='width:" + o.width + ";height:" + o.height + ";'>" +
+                    "<thead><tr></tr></thead><tbody></tbody>" +
+                    "</table>" +
+                    "<span id='dis_checkedItems' style='display:none;'></span>");
+
 	            var dataArray = new Array();
 	            for (d in o.data[0]) {
 	                dataArray.push(d);
 	            }
 	            dataArray.sort(this._sortNum).reverse();
-	            var g = 0;
-	            for (var c in o.columns) {
-	                $("#" + tableId + "_tableSorter thead tr").append("<td>" + c + "</td>");
-	                g++;
+
+                //生成表头
+	            var g, short_g_head;
+	            for (var j in o.columns) {
+	                g = o.columns[j];
+	                var g_head = g["head"];
+	                var g_maxLength = g["maxLength"];
+	                if (g_head.length > g_maxLength && g_maxLength !=0) {
+	                    short_g_head = g_head.substring(0, g_maxLength) + "...";
+	                } else {
+	                    short_g_head = g_head;
+	                }
+	                var g_context = g["context"];
+	                var g_head_align = g["head_align"];
+	                var g_v_align = g["v_align"];
+	                var g_h_align = g["h_align"];
+	                var g_height = g["height"];
+	                var g_width = g["width"];
+	                $("#" + tableId + "_tableSorter thead tr").append(
+                        "<td style='text-align: " + g_head_align
+                        + "; vertical-align: " + g_v_align
+                        + "; height: " + g_height
+                        + "; width: " + g_width + ";padding-left: 2px; padding-right: 2px;padding-top:5px;padding-bottom:5px;'>"
+                        + "<span style='cursor:default;' title='" + g_head + "'>" + short_g_head + "</span>" +
+                        "</td>"
+                        );
 	            }
+
+                //生成表格
+	            var v, short_v_head;
 	            for (var i = 0; i < o.data.length; i++) {
-	                var v;
-	                var str = "";
 	                $("#" + tableId + "_tableSorter tbody").append("<tr></tr>"); //行
 	                for (var c in o.columns) {
+	                    var isLink = false;
 	                    v = o.columns[c];
-	                    if ((typeof v == 'object') && v.constructor == Array) {
-	                        for (var m = 0; m < v.length; m++) {
-	                            str += v[m];
-	                            for (var r = 0; r < dataArray.length; r++) {
-	                                var t = "#" + dataArray[r];
-	                                str = str.replace(t, o.data[i][dataArray[r]]);
-	                            }
-	                        }
-	                    } else {
-	                        for (var r = 0; r < dataArray.length; r++) {
-	                            var t = "#" + dataArray[r];
-	                            v = v.replace(t, o.data[i][dataArray[r]]);
-	                        }
-	                        str = v;
+	                    var v_head = v["head"];
+	                    var v_context = v["context"];
+	                    var v_maxLength = v["maxLength"];
+	                    var v_head_align = v["head_align"];
+	                    var v_v_align = v["v_align"];
+	                    var v_h_align = v["h_align"];
+	                    var v_height = v["height"];
+	                    var v_width = v["width"];
+	                    for (var r = 0; r < dataArray.length; r++) {
+	                        var t = "#" + dataArray[r];
+	                        v_context = v_context.replace2(t, o.data[i][dataArray[r]]);
 	                    }
-	                    $("#" + tableId + "_tableSorter tbody tr:eq(" + i + ")").append("<td>" + str + "</td>"); //列 
-	                    str = "";
+                        //排除操作列
+	                    if (v_context.length > v_maxLength && !v_context.startsWith("<") && v_maxLength!=0) {
+	                        short_v_head = v_context.substring(0, v_maxLength) + "...";
+	                    } else {
+	                        short_v_head = v_context;
+	                        if(v_context.startsWith("<")){
+	                            isLink = true;
+	                        }
+	                    }
+	                    if (isLink == true) {
+	                        $("#" + tableId + "_tableSorter tbody tr:eq(" + i + ")").append("<td style='text-align: " + v_h_align
+                            + "; vertical-align: " + v_v_align
+                            + "; height: " + v_height
+                            + "; width: " + v_width + ";padding-left: 2px; padding-right: 2px;padding-top:5px;padding-bottom:5px;'><span>" + v_context + "</span></td>"); //列 
+	                    } else {
+	                        $("#" + tableId + "_tableSorter tbody tr:eq(" + i + ")").append("<td style='text-align: " + v_h_align
+                            + "; vertical-align: " + v_v_align
+                            + "; height: " + v_height
+                            + "; width: " + v_width + ";padding-left: 2px; padding-right: 2px;padding-top:5px;padding-bottom:5px;'><span style='cursor:default;' title='" + v_context + "'>" + short_v_head + "</span></td>"); //列 
+	                    }
 	                }
                     //隐藏域保存行ID
 	                $("#" + tableId + "_tableSorter tbody tr:eq(" + i + ") td:eq(0)").prepend("<span style='display:none;'>"+o.data[i]["ID"]+"</span>");
 	            }
+
+
+                //首列添加图标
 	            var table = $("#" + tableId + "_tableSorter");
+	            table.find('tr').find('td:eq(0)').find("span:eq(0)").before("<span class='jui-table-columnHeadImage'></span>");
 
+	            //*********表格行样式切换
 	            var $tr = table.children("tbody").find('tr');
-
-	            table.find("td").css({ "padding-top": "5px", "padding-bottom": "5px" });
-	            table.find('tr').find('td:eq(0)').append("<img style='float:left;width:20px;height:20px;margin-left:5px;margin-right:5px;' src='" + image + "'/>");
 	            //给tr设置LOCK、初始颜色,设置tr默认为没单击,设置背景色
-	            $tr.attr('Lock', 'false').css('background-color', o.outColor);
+	            $tr.attr('Lock', 'false').addClass("jui-table-out");
 	            //设置表格样式
 	            $tr.each(function (i) {
 	                var op = $(this); //tr对象
 	                //鼠标悬停事件
 	                op.hover(function () {
-	                    if (o.onClick) {//如果开启点击
-	                        if (op.attr('Lock') == 'false') { op.css('background-color', o.overColor); }
-	                    }
-	                    else {
-	                        op.css('background-color', o.overColor);
+	                    if (op.attr('Lock') == 'false') {
+	                        op.removeClass("jui-table-out");
+	                        op.addClass("jui-table-over");
 	                    }
 	                },
                     function () {
-                        if (o.onClick) {
-                            if (op.attr('Lock') == 'false') { op.css('background-color', o.outColor); }
-                        }
-                        else {
-                            op.css('background-color', o.outColor);
+                        if (op.attr('Lock') == 'false') {
+                            op.removeClass("jui-table-over");
+                            op.addClass("jui-table-out");
                         }
                     });
-	                //单击事件
-	                if (o.onClick) {
-	                    op.click(function () {
-	                        $tr.css('background-color', o.outColor); //全部tr初始化未覆盖时候的颜色
-	                        $tr.attr('Lock', 'false'); //全部tr初始化为未锁定状态
-	                        op.attr('Lock', 'true'); //选中tr变更为锁定状态
-	                        if (op.attr('Lock') == 'true') { op.css('background-color', o.onClickColor); } //选中tr颜色并更为选择状态颜色
-	                    });
-	                }
+	                op.mousedown(function () {
+	                    $tr.removeClass("jui-table-over");
+	                    $tr.removeClass("jui-table-click");
+	                    $tr.addClass("jui-table-out"); //全部tr初始化未覆盖时候的颜色
+	                    $tr.attr('Lock', 'false'); //全部tr初始化为未锁定状态
+	                    op.attr('Lock', 'true'); //选中tr变更为锁定状态
+	                    if (op.attr('Lock') == 'true') { op.addClass("jui-table-click"); } //选中tr颜色并更为选择状态颜色
+	                });
 	            });
+                //*********
+
 	            table.children("thead").find("td").css("background", "#094ab2");
 	            table.find('tr:eq(0)').css("color", "#ffffff");
 	        }
+	    },
+        //返回选中行数据
+	    currentData: function () {
+	        var o = this.options;
+	        var checkedRoll="";
+	        var checkedRollId = $(this.element).find(".jui-table-click").find("td:eq(0)").find("span:eq(1)").text();
+	        for (var i = 0; i < o.data.length; i++) {
+	            if (o.data[i]["ID"] == checkedRollId) {
+	                checkedRoll = o.data[i];
+	            }
+	        }
+	        return checkedRoll;
 	    },
 
 	    _init: function () {
