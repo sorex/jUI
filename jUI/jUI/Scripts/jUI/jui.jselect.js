28cb86a463083bd998e853b55a3e348d336a4755
Leidc <l15871437346@163.com>
2012-11-07 11:43:06 +0800

    单选下拉框和多选下拉框 融合后的提交

--- jUI/jUI/Scripts/jUI/jui.jselect.js
+++ jUI/jUI/Scripts/jUI/jui.jselect.js
@@ -20,36 +20,30 @@
 * Description: 普通的select
 */
 
-//item: [{ name: "Id", text: "编号" },
-//				{ name: "Age", text: "年龄", sort: "desc" },
-//				{ name: "finishData", text: "完成日期", sort: "asc" },
-//				{ name: "size", text: "大小", sort: "desc" }
-/*
-* options:
-*		item ：
-*				name: 表示排序字段的名称  text: 字段显示的值 sort:排序方式
+/*item:		[{ value: "China", text: "中国" }, value:表示实际值  text：表示文本显示的值  selected 表示是否选中
+*			{ value: " Japan", text: "日本" },
+*			{ value: "America", text: "美国",selected: "selected" },
+*			{ value: "Korea  ", text: "朝鲜" },
+*			{ value: "Corea  ", text: "韩国" }
+*			]
+*		
+*model:default（multiple） model表示是单选框还是多选框 default表示单选  multiple表示多选
 *
-*		onItemClick:回调函数 项选择之后触发
-*		
-* event
-*		
-*		
-*	    
+*placeholder: "请选择..."  placeholder表示没有选择前的提示语 默认为 请选择...	
+*	
+*width:150px   width表示select选择框的宽度 默认为150px
+*
+*onSelectChange:null  onSelectChange表示 选项的点击事件
 */
 
 (function ($, undefined) {
 	$.widget("jui.jSelect",
 	{
 		options: {
-			item: [{ value: "China", text: "中国" },
-					{ value: " Japan", text: "日本" },
-					{ value: "America", text: "美国" },
-					{ value: "Korea  ", text: "朝鲜" },
-					{ value: "Corea  ", text: "韩国" }
-			],
-			model:"default",
+			item: null,
+			model: "default",
 			placeholder: "请选择",
-			width:"150px",
+			width: "150px",
 			onSelectChange: null
 		},
 
@@ -59,31 +53,41 @@
 			o = this.options;
 			var tagId = this.element.attr("id");/*目标的id*/
 			var width, palceHoler;
-			var selectHtml='';
+			var selectHtml = '';
 			var selectId = tagId + "_select";
-			
-		
-			if (o.model != "multiple") { /*多选功能的select*/
-				selectHtml += "<select id=" + selectId + " data-placeholder=" + o.placeholder + " class='chzn-select' style='width:"+o.width+";' tabindex='2'><option></option>";
-			} else {
-			
-				selectHtml += "<select data-placeholder=" + o.placeholder + " class='chzn-select' multiple style='width:" + o.width + ";' tabindex='4'><option></option>";
-			}
-			for (var i = 0; i < o.item.length; i++) {
-				selectHtml += "<option value="+o.item[i].value+">"+o.item[i].text+"</option>";
-			}
-			selectHtml += "</select>";
-			$("#" + tagId).append(selectHtml);
-			$(".chzn-select").chosen();
-			if (o.model != "multiple") {
+
+			if (o.item != null) {
+
+				if (o.model != "multiple") { /*多选功能的select*/
+					selectHtml += "<select id=" + selectId + " data-placeholder=" + o.placeholder + " class='chzn-select' style='width:" + o.width + ";' tabindex='2'><option></option>";
+				} else {
+
+					selectHtml += "<select id=" + selectId + " data-placeholder=" + o.placeholder + " class='chzn-select' multiple style='width:" + o.width + ";' tabindex='4'><option></option>";
+				}
+				for (var i = 0; i < o.item.length; i++) {
+					if (o.item[i].selected == "selected") {
+						selectHtml += "<option selected value=" + o.item[i].value + ">" + o.item[i].text + '　' + "</option>";
+					} else {
+						selectHtml += "<option value=" + o.item[i].value + ">" + o.item[i].text + '　' + "</option>";
+					}
+
+				}
+				selectHtml += "</select>";
+				$("#" + tagId).append(selectHtml);
+				$(".chzn-select").chosen();
+				//alert(selectHtml);
+
 				$("#" + selectId).change(function () {
 					self.options.onSelectChange($(this).val(), $(this).find("option:selected").text());
 				});
-			}else{
-				
+
+			} else {
+
+				$("#" + tagId).append("<span style='color:red;'>错误！！！请检查数据源</span>");
+
 			}
-		
-		
+
+
 		},
 
 
