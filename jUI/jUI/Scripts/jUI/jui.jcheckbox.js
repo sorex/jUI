1a26bc1db586a499203e4a6b39c51e1a4a29cd5c
Leidc <l15871437346@163.com>
2012-11-09 17:50:15 +0800

    checkbox的鼠标以上动作的修改完成

--- jUI/jUI/Scripts/jUI/jui.jcheckbox.js
+++ jUI/jUI/Scripts/jUI/jui.jcheckbox.js
@@ -1,24 +1,45 @@
 ﻿$(function () {
-	$(":checkbox").after("<span class='jui-checkbox'></span>").click(function (event) {
+	$(":checkbox").after("<span class='jui-checkbox'></span>").click(function (event) {//checkbox的点击事件
 		if ($(this).is(':checked')) {
+
 			$(this).next().addClass("jui-checkbox-checked");
 			$(this).checked = true;
+			$(this).next().removeClass("jui-checkbox-checked-hover");
+			$(this).next().removeClass('jui-checkbox-checked-checked');
 		}
 		else {
+
+			$(this).next().removeClass('jui-checkbox-checked-checked');
 			$(this).next().removeClass('jui-checkbox-checked');
+			$(this).next().addClass("jui-checkbox-checked-hover");
 			$(this).checked = false;
 		}
 		event.stopPropagation();
-	}).hide().next().click(function (event) {
+
+	}).hide().next().click(function (event) { //背景图片点击事件
 		var myObject = document.getElementById($(this).prev().attr("id"));
 		if (myObject.checked == true) {
-			$(this).removeClass('jui-checkbox-checked')
+			$(this).removeClass('jui-checkbox-checked');
 			myObject.checked = false;
 		} else {
-
 			myObject.checked = true;
 			$(this).addClass('jui-checkbox-checked')
 		}
+		event.stopPropagation();
+
+	}).next().mouseover(function (event) {
+		$(this).addClass('jui-checkbox-laber');
+
+		if ($(this).prev().attr("class") == 'jui-checkbox') {
+			$(this).prev().addClass('jui-checkbox-checked-hover');
+		} else {
+			$(this).prev().addClass('jui-checkbox-checked-checked');
+		}
+
+		event.stopPropagation();
+	}).mouseout(function (event) {
+		$(this).prev().removeClass('jui-checkbox-checked-hover');
+		$(this).prev().removeClass('jui-checkbox-checked-checked');
 		event.stopPropagation();
 	});
 
