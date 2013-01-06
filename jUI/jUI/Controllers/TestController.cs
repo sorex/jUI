4f448a4a7cad0226688d634e3a44fca185dc74cf
sorex <sorex1984@gmail.com>
2013-01-06 09:44:10 +0800

    添加测试页面

--- jUI/jUI/Controllers/TestController.cs
+++ jUI/jUI/Controllers/TestController.cs
@@ -6,22 +6,43 @@ using System.Web.Mvc;
 
 namespace jUI.Controllers
 {
-    public class TestController : Controller
-    {
-        //
-        // GET: /Test/
+	public class TestController : Controller
+	{
+		//
+		// GET: /Test/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
+		public ActionResult Index()
+		{
+			return View();
+		}
 
 		public ActionResult layout()
 		{
 			return View();
 		}
 
-		public ActionResult datapager()
+		#region 一期
+		public ActionResult leftMenu()
+		{
+			return View();
+		}
+
+		public ActionResult button()
+		{
+			return View();
+		}
+
+		public ActionResult checkbox()
+		{
+			return View();
+		}
+
+		public ActionResult radio()
+		{
+			return View();
+		}
+
+		public ActionResult headMenu()
 		{
 			return View();
 		}
@@ -31,7 +52,7 @@ namespace jUI.Controllers
 			return View();
 		}
 
-		public ActionResult tip()
+		public ActionResult datapager()
 		{
 			return View();
 		}
@@ -40,5 +61,73 @@ namespace jUI.Controllers
 		{
 			return View();
 		}
+
+		public ActionResult tip()
+		{
+			return View();
+		}
+
+		public ActionResult select()
+		{
+			return View();
+		}
+
+		public ActionResult sortSelect()
+		{
+			return View();
+		}
+
+		public ActionResult multiSelect()
+		{
+			return View();
+		}
+
+		public ActionResult link()
+		{
+			return View();
+		}
+
+		public ActionResult linkButton()
+		{
+			return View();
+		}
+		#endregion
+
+		#region 二期
+		public ActionResult checkTable()
+		{
+			return View();
+		}
+		public ActionResult radioTable()
+		{
+			return View();
+		}
+		public ActionResult tree()
+		{
+			return View();
+		}
+		public ActionResult text()
+		{
+			return View();
+		}
+		public ActionResult checkTree()
+		{
+			return View();
+		}
+		public ActionResult radioTree()
+		{
+			return View();
+		}
+		public ActionResult search()
+		{
+			return View();
+		}
+		public ActionResult scrollbar()
+		{
+			return View();
+		}
+
+		#endregion
+
 	}
 }
